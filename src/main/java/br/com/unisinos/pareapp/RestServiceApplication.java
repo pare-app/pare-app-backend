c3f0adec5065eafc2f9e4a400cebacd09a2bc2e1
Rafael Correa Alves <rafael.correa@dbccompany.com.br>
2022-04-22 10:21:42 -0300

    Changed open api server url to avoid wrong url requests

--- src/main/java/br/com/unisinos/pareapp/RestServiceApplication.java
+++ src/main/java/br/com/unisinos/pareapp/RestServiceApplication.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @SecurityScheme(name = "pare-app-api", scheme = "bearer", bearerFormat = "JWT",type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
-@OpenAPIDefinition(info = @Info(title = "PareApp API", version = "0.0", description = "Backend API do Sistema PareApp"))
+@OpenAPIDefinition( info = @Info(title = "PareApp API", version = "0.0", description = "Backend API do Sistema PareApp"),
+                    servers = {@Server(url = "/", description = "Default Server URL")})
 public class RestServiceApplication {
 
     public static void main(String[] args) {
